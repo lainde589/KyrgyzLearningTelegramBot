ac9458f0e663f736da715458ffa280dc2010c837
Ariet Muzirapov <procoder589@gmail.com>
2024-02-05 13:08:22 +0600

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,51 @@
-import { Telegraf, Context, session } from 'telegraf';
-import sqlite from 'sqlite3';
-import { getPhrase, addPhrase, deletePhrase } from './modules/phrasesHandler';
-
+import { Telegraf, Context } from 'telegraf';
 
 require('dotenv').config();
 const bot: Telegraf = new Telegraf(process.env.BOT_TOKEN || '');
 
 
 
-
-/*
-    ? SQLite Database Setup
-*/
-const db: sqlite.Database = new sqlite.Database('kyrgyz_lexicon.db');
-db.run('CREATE TABLE IF NOT EXISTS phrases (id INTEGER PRIMARY KEY AUTOINCREMENT, phrase TEXT, translation TEXT)');
-
-bot.use(session());
+bot.command('start', ctx => handleStartCommand(ctx));
+bot.command('help', ctx => handleHelpMessage(ctx));
+bot.command('author', ctx => handleAuthorCommand(ctx));
 
 
 
+function handleStartCommand(ctx: Context): void {
+    const startCommandText: string = 
+        'Hello, let\'s start your journey to learning the Kyrgyz language' +
+        '\nJust tap on one of the sections below, and go ahead!' +
+        '\n\nTap /help to get full list of commands';
+    
+    ctx.reply(startCommandText, {
+        reply_markup: {
+            resize_keyboard: true,
+            inline_keyboard: [
+                [{ text: 'Alphabet 🔡', callback_data: 'alphabet' }],
+                [{ text: 'Phonetics 🔊', callback_data: 'phonetics' }],
+                [{ text: 'Grammar 📖', callback_data: 'grammar' }],
+                [{ text: 'Vocabulary 📚', callback_data: 'vocab' }]
+            ]
+        }
+    });
+}
 
-bot.command('start', ctx => {
-    ctx.reply('test');
-});
-bot.command('getphrase', async (ctx: Context) => getPhrase(ctx));
+function handleHelpMessage(ctx: Context): void {
+    const helpCommandText: string =  
+        '/start - start the tutorial again\n' +
+        '/author - information about the author\n'
+        
+    ctx.reply(helpCommandText);
+}
 
-// !! COMMANDS BELOW CAN BE USED BY DEVELOPERS ONLY !!
-bot.command('addphrase', async (ctx: Context) => addPhrase(ctx));
-bot.command('dltphrase', async (ctx: Context) => deletePhrase(ctx));
+function handleAuthorCommand(ctx: Context): void {
+    const authorCommandText: string =
+        'If you have any questions or need assistance, as well as suggestions for developing the bot,\n' +
+        'feel free to contact me on Telegram: @lainde_so2\n\n' +
+        'Source code of the bot here: https://github.com/laindeso2/KyrgyzLearningTelegramBot';
+
+    ctx.reply(authorCommandText);
+}
 
 
 
@@ -36,6 +54,3 @@ bot.command('dltphrase', async (ctx: Context) => deletePhrase(ctx));
 //* dropPendingUpdates: true - bot won't reply if you write to it while it's off
 bot.launch({dropPendingUpdates: true})
     .then(() => console.log(`Bot started (${new Date().toLocaleTimeString()})`));
-
-
-export default db;
