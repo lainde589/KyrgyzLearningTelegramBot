ba67aa8dfbb4fad21b78122aba4b1b792509753a
Ariet Muzirapov <procoder589@gmail.com>
2024-02-06 19:11:19 +0600

    added more content

---
+++ src/db/content/alphabet.ts
@@ -0,0 +1,68 @@
+export const alphabetText: string =
+`
+The Kyrgyz Alphabet
+
+Аа  [ ɑ ] — (u)pon
+Бб  [ b ] — (b)all
+Вв  [ v ] — (v)oid
+Гг  [ g/ʁ ] — (g)ain or French R
+Дд  [ d ] — (d)elta
+Ее  [ je/e ] — (ye)t or l(e)t
+Ёё  [ jo ] — (yo)rk
+Жж  [ dʒ/ʒ ] — (j)ump or plea(s)ure
+Зз  [ z ] — (z)oom
+Ии  [ i ] — s(ee)d
+Йй  [ j ] — to(y)
+Кк  [ k ] — sch(oo)l or "k" in the throat
+Лл  [ l ] — (l)ate
+Мм  [ m ] — (m)uch
+Нн  [ n ] — (n)est
+Ӊӊ  [ ŋ ] — lo(ng)
+Оо  [ o ] — (o)kay
+Өө  [ ø ] — (ea)rn
+Пп  [ p ] — s(p)eech
+Рр  [ r ] — rolled R
+Сс  [ s ] — (s)lip
+Тт  [ t ] — s(t)ick
+Уу  [ u ] — b(oo)k
+Үү  [ y ] — n(ew)
+Фф*  [ f ] — (f)ood
+Хх*  [ χ ] — German "do(ch)"
+Цц*  [ ts ] — ca(ts)
+Чч  [ tʃ ] — pun(ch)
+Шш  [ ʃ ] — (sh)ort
+Щщ*  [ ɕ ] — (sh)eep
+Ъъ*  [ " ] — a hard sign
+Ыы  [ ɯ ] — cous(i)n
+Ьь*  [ ' ] — a soft sign
+Ээ  [ e ] — l(e)t
+Юю  [ ju ] — (you)th
+Яя  [ ja ] — (ya)wn
+`;
+
+
+
+export const alphabetDetailedText: string =
+`
+The Kyrgyz Cyrillic alphabet is the official writing system of the Kyrgyz language.
+
+It's based on the Russian Cyrillic alphabet, with the addition of three additional letters: Ңң, Өө, and Үү.
+
+The alphabet consists of 36 letters (33 from the Russian alphabet + 3 additional), that includes:
+
+📍 22 consonants (Бб, Вв, Гг, Дд, Жж, Зз, Йй, Кк, Лл, Мм, Нн, Ӊӊ, Пп, Рр, Сс, Тт, Фф, Хх, Цц, Чч, Шш, Щщ)
+
+📍 8 vowels (Аа, Ыы, Ее, Ии, Оо, Өө, Уу, Үү)
+There are 4 more letters (Яя, Ее, Ёё, and Юю) that technically represent vowel sounds, however they're considered as combinations of other letters (Яя = Йй + Аа, Ёё = Йй + Оо, and so on) in Kyrgyz.
+
+📍 2 other letters (Ъ ъ, Ь ь)
+These letters don't represent individual sounds but modify the pronunciation of preceding consonants.
+
+💡 The letters marked with an asterisk * (Фф, Хх, Цц, Щщ, Ъъ, Ьь) are rarely used in Kyrgyz language and mainly appear in loanwords.
+
+👇 Here's a full tutorial on Kyrgyz alphabet 👇
+`;
+
+
+
+export const alphabetTutorialUrl: string = 'https://youtu.be/UrKCyUECeZI?feature=shared';
